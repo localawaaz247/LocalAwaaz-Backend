a31d00981479904a5f3aa082305b600fee695db0
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-01-08 01:54:43 +0530

    Connected DB and made dotenv

---
+++ src/server/database/connectDB.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+const connectDB = async () => {
+    try {
+        await mongoose.connect(process.env.MONGO_CONNECT_URL);
+        console.log("DB is ONLINE");
+    }
+    catch (err) {
+        console.log("DB Connection failed:", err.message)
+        throw new Error("DB OFFLINE")
+    }
+}
+module.exports = connectDB
---
+++ src/server/server.js
@@ -0,0 +1,27 @@
+const express = require('express');
+require('dotenv').config()
+const connectDB = require('./database/connectDB');
+const app = express();
+app.use("/", (req, res) => {
+    res.send("hello");
+})
+
+
+
+const startServer = async () => {
+    try {
+        await connectDB();
+        const server = app.listen(process.env.PORT, () => {
+            console.log("Server ONLINE");
+        })
+        server.on("error", (err) => {
+            console.error("Server encountered an error:", err.message);
+        });
+    }
+    catch (err) {
+        console.log(err.message);
+    }
+
+}
+startServer();
+
