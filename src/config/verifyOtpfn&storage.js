43a1d9ac330b60c728371e0f0cfcaa225e10a7f9
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-01-08 19:15:27 +0530

    enryption of otp in DB

--- src/config/verifyOtpfn&storage.js
+++ src/config/verifyOtpfn&storage.js
@@ -1,5 +1,5 @@
 const OtpModel = require("../models/Otp");
-
+const bcrypt = require('bcrypt')
 const verifyOtp = async (email, enteredOtp) => {
     const record = await OtpModel.findOne({ email });
     if (!record) {
@@ -12,7 +12,7 @@ const verifyOtp = async (email, enteredOtp) => {
     if (attempts >= 5) {
         throw new Error("Maximum attempts reached! Try again after some time");
     }
-    if (otp != enteredOtp) {
+    if (!bcrypt.compare(record.otp, enteredOtp)) {
         record.attempts += 1;
         await record.save();
         throw new Error("Wrong OTP entered");
--- src/routes/userRouter.js
+++ src/routes/userRouter.js
@@ -4,6 +4,7 @@ const validateSignUpData = require('../../utils/validateSignUpData');
 const verifyOtp = require('../config/verifyOtpfn&storage');
 const validate = require('validator');
 const OtpModel = require('../models/Otp');
+const bcrypt = require('bcrypt')
 const userRouter = express.Router();
 
 userRouter.post('/user/signup', async (req, res) => {
@@ -23,18 +24,19 @@ userRouter.post('/user/send-otp', async (req, res) => {
             return res.status(400).json({ message: "Enter Valid email id" });
         }
         const generatedOtp = Math.floor(100000 + Math.random() * 900000).toString();
+        const hashedOtp = await bcrypt.hash(generatedOtp, 10);
         await sendMail({ email, generatedOtp });
         const expiresAt = new Date(Date.now() + 10 * 60 * 1000)
         const record = await OtpModel.findOne({ email });
         if (record) {
-            record.otp = generatedOtp;
+            record.otp = hashedOtp;
             record.expiresAt = expiresAt;
             await record.save();
         }
         else {
-            await OtpModel.create({ email, otp: generatedOtp, expiresAt });
+            await OtpModel.create({ email, otp: hashedOtp, expiresAt });
         }
-        res.status(200).json({ success: true, message: "OTP sent on your email id" });
+        res.status(200).json({ success: true, message: "If email exists, OTP has been sent" });
     } catch (err) {
         res.status(400).json({ success: false, message: "Enter valid email id" });
     }
@@ -48,7 +50,7 @@ userRouter.post('/user/verify-otp', async (req, res) => {
         if (!email || !otp) return res.status(400).json({ message: 'Email and OTP required' });
 
         await verifyOtp(email, otp);
-
+        
         res.status(200).json({ success: true, message: 'OTP verified successfully' });
     } catch (err) {
         res.status(400).json({ success: false, message: err.message });
@@ -58,20 +60,21 @@ userRouter.post('/user/resend-otp', async (req, res) => {
     try {
         const { email } = req.body;
         if (!validate.isEmail(email)) {
-            return res.status(500).json({ message: "Enter Valid email id" });
+            return res.status(400).json({ message: "Enter Valid email id" });
         }
         const generatedOtp = Math.floor(100000 + Math.random() * 900000).toString();
+        const hashedOtp = await bcrypt.hash(generatedOtp, 10);
         const record = await OtpModel.findOne({ email });
         if (!record) {
-            return res.status(404).json({ success: false, message: "Enter valid email id" })
+            return res.status(400).json({ success: false, message: "Enter valid email id" })
         }
         await sendMail({ email, generatedOtp });
         const expiresAt = new Date(Date.now() + 10 * 60 * 1000);
-        record.otp = generatedOtp;
+        record.otp = hashedOtp;
         record.attempts = 0;
         record.expiresAt = expiresAt;
         await record.save();
-        res.status(200).json({ success: true, message: "OTP sent on your email id" });
+        res.status(200).json({ success: true, message: "If email exists, OTP has been sent" });
 
     } catch (err) {
         res.status(400).json({ success: false, message: "Enter valid email id" });
