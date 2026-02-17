be3cce791722f63c19e2c2c6483a3b5d75722140
Ankit Mani Pandey <ankitpandey78600@gmail.com>
2026-02-17 13:45:59 +0530

    limit increased of json

--- src/server/server.js
+++ src/server/server.js
@@ -26,7 +26,8 @@ app.use(passport.session());
 
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(cookieParser());
 
 app.set("trust proxy", 1);
