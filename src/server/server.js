22543dc9cab8f61ef6be806ec67a5e1ac7036759
Ankit Mani Pandey <edumusicindia.tech@gmail.com>
2026-06-13 15:40:41 +0530

    fixed the routes

--- src/server/server.js
+++ src/server/server.js
@@ -97,7 +97,7 @@ app.use('/', contactRouter);
 app.use('/', mediaRouter);
 app.use('/', lokAiRouter);
 app.use('/', adminRouter);
-app.use('/', visitsRouter);
+app.use('/api/visits', visitsRouter);
 
 // A simple route to keep the server awake
 app.get('/ping', (req, res) => {
