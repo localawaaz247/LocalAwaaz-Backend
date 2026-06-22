20be4aa97aed583be49c3dbe4b66bd22d393a885
Ankit Mani Pandey <edumusicindia.tech@gmail.com>
2026-06-22 19:47:08 +0530

    made apk logic

--- src/models/AppRelease.js
+++ src/models/AppRelease.js
@@ -5,6 +5,7 @@ const appReleaseSchema = new mongoose.Schema({
     versionName: { type: String, required: true }, // e.g., "1.0.5"
     versionCode: { type: Number, required: true }, // e.g., 5 (Important for Android comparisons)
     downloadUrl: { type: String, required: true }, // The Cloudflare R2 public URL
+    appUpdateAvailable: { type: Boolean, default: false },
     fileSize: { type: String }, // e.g., "24.5 MB" - Good for UX
     releaseNotes: { type: String }, // e.g., "Fixed FCM push notifications"
     isMandatory: { type: Boolean, default: false }, // For future "Force Update" feature
